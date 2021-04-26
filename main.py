f65a86fa99dc50330712fa59bab66c90ac0b8acd
Akashkumar2001 <55625389+Akashkumar2001@users.noreply.github.com>
2021-04-26 09:52:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,17 +22,79 @@ def speak(word):
 bot = ChatBot("My Bot")
 
 convo = [
-    'hello',
+   'hello',
     'hi there !',
     'what is your name ?',
-    'My name is Bot , i am created by Durgesh',
+    
     'how are you ?',
     'I am doing great these days',
     'thank you',
     'In which city you live ?',
-    'I live in lucknow',
+    
     'In which language you talk?',
-    ' I mostly talk in english'
+    ' I mostly talk in english',
+    'What is covid-19?',
+    'COVID-19 is the disease caused by a new coronavirus called SARS-CoV-2.  WHO first learned of this new virus on 31 December 2019, following a report of a cluster of cases of ‘viral pneumonia’ in Wuhan, People’s Republic of China.',
+    'what are the symptoms of covid-19?',
+    '''The most common symptoms of COVID-19 are
+
+Fever
+Dry cough
+Fatigue
+Other symptoms that are less common and may affect some patients include:
+
+Loss of taste or smell,
+Nasal congestion,
+Conjunctivitis (also known as red eyes)
+Sore throat,
+Headache,
+Muscle or joint pain,
+Different types of skin rash,
+Nausea or vomiting,
+Diarrhea,
+Chills or dizziness.
+ 
+
+Symptoms of severe COVID‐19 disease include:
+
+Shortness of breath,
+Loss of appetite,
+Confusion,
+Persistent pain or pressure in the chest,
+High temperature (above 38 °C).
+Other less common symptoms are:
+
+Irritability,
+Confusion,
+Reduced consciousness (sometimes associated with seizures),
+Anxiety,
+Depression,
+Sleep disorders,
+More severe and rare neurological complications such as strokes, brain inflammation, delirium and nerve damage.
+People of all ages who experience fever and/or cough associated with difficulty breathing or shortness of breath, chest pain or pressure, or loss of speech or movement should seek medical care immediately. If possible, call your health care provider, hotline or health facility first, so you can be directed to the right clinic.''',
+   
+    'who are most at risk of severe illness from covid-19?',
+    '''Some people who have had COVID-19, whether they have needed hospitalization or not, continue to experience symptoms, including fatigue, respiratory and neurological symptoms.
+ ''',
+    'how we can protect others and ourselves if we do not know who is infected?',
+    'Stay safe by taking some simple precautions, such as physical distancing, wearing a mask, especially when distancing cannot be maintained, keeping rooms well ventilated, avoiding crowds and close contact, regularly cleaning your hands, and coughing into a bent elbow or tissue. Check local advice where you live and work. Do it all!',
+    'When should i get a test for covid-19?',
+    '''Anyone with symptoms should be tested, wherever possible. People who do not have symptoms but have had close contact with someone who is, or may be, infected may also consider testing – contact your local health guidelines and follow their guidance.  
+
+While a person is waiting for test results, they should remain isolated from others. Where testing capacity is limited, tests should first be done for those at higher risk of infection, such as health workers, and those at higher risk of severe illness such as older people, especially those living in seniors’ residences or long-term care facilities.''',
+    'what test should i get to see if i have covid-19?',
+    '''In most situations, a molecular test is used to detect SARS-CoV-2 and confirm infection. Polymerase chain reaction (PCR) is the most commonly used molecular test. Samples are collected from the nose and/or throat with a swab. Molecular tests detect virus in the sample by amplifying viral genetic material to detectable levels. For this reason, a molecular test is used to confirm an active infection, usually within a few days of exposure and around the time that symptoms may begin. ''',
+    '''what about rapid test?''',
+    '''Rapid antigen tests (sometimes known as a rapid diagnostic test – RDT) detect viral proteins (known as antigens). Samples are collected from the nose and/or throat with a swab. These tests are cheaper than PCR and will offer results more quickly, although they are generally less accurate. These tests perform best when there is more virus circulating in the community and when sampled from an individual during the time they are most infectious. ''',
+    'what should i do if i have covid-19 symptoms?',
+    '''If you have any symptoms suggestive of COVID-19, call your health care provider or COVID-19 hotline for instructions and find out when and where to get a test, stay at home for 14 days away from others and monitor your health.
+
+If you have shortness of breath or pain or pressure in the chest, seek medical attention at a health facility immediately. Call your health care provider or hotline in advance for direction to the right health facility.
+
+If you live in an area with malaria or dengue fever, seek medical care if you have a fever.
+
+If local guidance recommends visiting a medical centre for testing, assessment or isolation, wear a medical mask while travelling to and from the facility and during medical care. Also keep at least a 1-metre distance from other people and avoid touching surfaces with your hands.  This applies to adults and children.''',
+
 ]
 
 trainer = ListTrainer(bot)
